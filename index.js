4e82ad84c5713f6146b8e0ba7a510e5a0d89e5c2
Sudhanshu Tiwari <sudhanshu.98t@gmail.com>
2025-01-24 21:46:06 +0530

    Remove syntax error

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/connectDB")
-const  = require('cors')
+const cors = require('cors')
 
 const authRouter = require('./routes/auth')
 const adminRouter = require('./routes/admin')
